4153337bba32b73cb9b69766317a8e01d86811cd
Mark Fine <mark.fine@gmail.com>
2018-06-17 22:56:22 -0700

    v4

--- Shakefile.hs
+++ Shakefile.hs
@@ -32,8 +32,8 @@ main = do
       -- List out projects under "projects", tracking the result as a shake dependency.
       projects <- getDirectoryDirs "projects"
 
-      -- Build the "dependencies" file for each project if necessary.
-      let dependencies = map (\project -> addExtension project "dependencies") projects
+      -- Build the "dependencies" file (without versions) for each project if necessary.
+      let dependencies = map (\project -> addExtension project "dependencies.versionless") projects
       need dependencies
 
       -- Merge "dependencies" files for each project.
@@ -46,3 +46,14 @@ main = do
 
       -- List external dependencies for project without base dependencies.
       cmd_ ("stack" :: String) (Cwd ("projects" </> dropExtension out)) (FileStdout out) [ "ls" :: String, "dependencies", "--external", "--no-include-base" ]
+
+
+
+    -- "*.dependencies.versionless" recipe - Collect dependencies for a project without versions.
+    "*.dependencies.versionless" %> \out -> do
+
+      -- Build the "dependencies" file for each project if necessary.
+      need [ dropExtension out ]
+
+      -- Cut off the version column.
+      cmd_ ("cut" :: String) (FileStdin (dropExtension out)) (FileStdout out) [ "-d" :: String, " ", "-f1" ]
